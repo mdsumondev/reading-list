d5b68fd4a820436a6c1e744bc02f0ee1ccc92cb1
Md.Sumon <sumonahamed5566@gmail.com>
2026-09-22 00:18:08 +0600

    Header

---
+++ src/app/Component/header.tsx
@@ -0,0 +1,86 @@
+import Link from "next/link";
+
+const Header = () => {
+  const link = (
+    <>
+      <li>
+        <Link
+          href="/"
+          className="text-black text-[18px] hover:text-[#23BE0A] px-[28px] py-[18px] hover:rounded-md hover:font-medium hover:border-[#23BE0A] hover:border-1 hover:bg-transparent"
+        >
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/"
+          className="text-black text-[18px] hover:text-[#23BE0A] px-[28px] py-[18px] hover:rounded-md hover:font-medium hover:border-[#23BE0A] hover:border-1 hover:bg-transparent"
+        >
+          Listed Books
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/"
+          className="text-black text-[18px] hover:text-[#23BE0A] px-[28px] py-[18px] hover:rounded-md hover:font-medium hover:border-[#23BE0A] hover:border-1 hover:bg-transparent"
+        >
+          Pages to Read
+        </Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                aria-label="Menu"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {link}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">Reading List</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{link}</ul>
+        </div>
+        <div className="navbar-end gap-x-5">
+          <Link
+            href="#"
+            className="px-[28px] py-[18px] rounded-sm bg-[#23BE0A] text-white text-[18px] font-medium"
+          >
+            Sign In
+          </Link>
+          <Link
+            href="#"
+            className="px-[28px] py-[18px] rounded-sm bg-[#59C6D2] text-white text-[18px] font-medium"
+          >
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
